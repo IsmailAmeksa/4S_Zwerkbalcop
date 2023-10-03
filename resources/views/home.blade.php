208ca7c8495ed21d537c2a4a22fca7675ee0f140
Ismail Ameksa <ismailameksa38@gmail.com>
2023-10-03 10:30:35 +0200

    homepage update
    aangemaakt zoals de eerste afbeelding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,30 @@
 @extends('layout')
 
 @section('nav')
-    <nav>
-        <a href="/" class="active">Home</a>
-        <a href="/tournaments">Toernooien</a>
-        <a href="/teams">Teams</a>
-        <a href="/players">Spelers</a>
-    </nav>
+<nav>
+    <a href="/" class="active">Home</a>
+    <a href="/tournaments">Toernooien</a>
+    <a href="/teams">Teams</a>
+    <a href="/players">Spelers</a>
+</nav>
 @endsection
 
 @section('content')
 
-    <h1>Test</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
+<h1>Dashboard</h1>
+<p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van J.K. Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam.</p>
+<div class="gridFlex">
+    <div class="gridFlexAmount">
+        <div class="Amount" id="amounttournaments">
+            <h2>Aantal aankomende toernooien:</h2>
+            <p>5</p>
+        </div>
+        <div class="Amount" id="amountTeams">
+            <h2>Totaal aantal teams</h2>
+            <p>26</p>
+        </div>
+    </div>
+    <img src="img\snitch.png">
+</div>
 
 @endsection
